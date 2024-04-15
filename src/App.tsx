7d0eef56ef04bd02d845c8bb9bfc02d922586fb5
MARIA EDUARDA SOUZA DIAS <dias.7980@aluno.pr.senac.br>
2024-04-15 11:26:14 -0300

    alterações dia 15/04

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import image from './res/Game galaxy.png'
 import mc4Image from './res/mc4.webp'
 import godImage from './res/download.png'
 import xboximage from "./res/640px-Xbox_one_logo.svg.jpg"
-import mc5image from "./res/mc5.jpg"
+import mc5image from "./res/nintendo.png"
 import mc7image from "./res/mc7.jpg"
 function App() {
   return(
@@ -33,19 +33,18 @@ function App() {
       </div>
       <div className='Center'>
         <div className='buttonsCatalogo'>
-         <div className='card' style={{background: `url(${mc4Image})`, backgroundSize: `cover`}}>
-            {/* <MyButton bgColor='white' altura='100px' largura='150px'><h2>Todas Categorias</h2></MyButton> */}
+         {/* <div className='card' style={{background: `url(${mc4Image})`, backgroundSize: `cover`}}> */}
+         <div className='cardA'>
+            <MyButton bgColor='white' altura='100px' largura='150px'><h2>Todas Categorias</h2></MyButton>
           </div>
           <div className='card' style={{background: `url(${godImage})`, backgroundSize: `cover`}}>
-           {/* <MyButton bgColor='white'  altura='100px' largura='100px'> */}
-            
-            {/* <h3>Playstation</h3> */}
-           {/* </MyButton> */}
+           <MyButton bgColor='transparent' > 
+            <h3>Playstation</h3></MyButton>
           </div>
           <div className='card'style={{background: `url(${xboximage})`, backgroundSize: `cover`}}>
            {/* <MyButton bgColor='white' altura='100px' largura='100px'><h2>Xbox</h2></MyButton> */}
          </div>
-          <div className='card' style={{background: `url(${mc5image})`, backgroundSize: `cover`}}>
+          <div className='card' style={{background: `url(${mc5image})`, backgroundSize: `97% 97%`}}>
             {/* <MyButton bgColor='white' altura='100px' largura='100px'><h2>Nintendo</h2></MyButton> */}
           </div>
           <div className='card' style={{background: `url(${mc7image})`, backgroundSize: `cover`}}>
@@ -53,7 +52,7 @@ function App() {
           </div>
         </div>
       </div>
-    </div>
+       </div>
   )
 }
 
