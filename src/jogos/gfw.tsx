3c23117db84a5e254e61e2eba163f3b5379e2d40
JOAO VICTOR SILVA <joao.34907@aluno.pr.senac.br>
2024-06-07 09:12:08 -0300

    07/06/2024/

--- src/jogos/gfw.tsx
+++ src/jogos/gfw.tsx
@@ -1,40 +1,40 @@
-import './jogos.css'
-import MyButton from '../MyButton'
-import Gfw from '../res/God_of_War_Ragnarök_capa.jpg'
+    import './jogos.css'
+    import MyButton from '../MyButton'
+    import Gfw from '../res/God_of_War_Ragnarök_capa.jpg'
 
 
 
- function gfw(){
-    return(
-        <div className='jogo'>
-            <div className='jogo-img' style={{background: `url(${Gfw})`, backgroundSize: `cover`}}></div>
-            <div className='P'>
-                <h1> God of War Ragnarök</h1>
-                <h2> 201.99</h2>
-               <h4> O jogo da início ao Ragnarok, </h4>
-               <h4> uma série de eventos que trazem os </h4>
-               <h4> fins dos tempos e retrata a</h4>
-               <h4> morte de alguns dos deuses nórdico.</h4>
-                <div className='button2'>
-                    <MyButton>
-                        <p>comprar agora</p>
-                    </MyButton>
-                    <div className='button3'>
-                        <MyButton>
-                        <p>adicionar ao carrinho</p>
-                        </MyButton>
-                        <div className='cometairo'>
-                                <h4>
-                                "A história entre Kratos e Atreus é uma das melhores que já vi em um jogo. A evolução dos personagens é incrível."
-                                </h4>
+    function gfw(){
+        return(
+            <div className='jogo'>
+                <div className='jogo-img' style={{background: `url(${Gfw})`, backgroundSize: `cover`}}></div>
+                <div className='P'>
+                    <h1> God of War Ragnarök</h1>
+                    <h2> 201.99</h2>
+                <h4> O jogo da início ao Ragnarok, </h4>
+                <h4> uma série de eventos que trazem os </h4>
+                <h4> fins dos tempos e retrata a</h4>
+                <h4> morte de alguns dos deuses nórdico.</h4>
+                    <div className='button2'>
+                            <MyButton>
+                                <p>comprar agora</p>
+                            </MyButton>
+                            <div className='button3'>
+                                <MyButton>
+                                <p>adicionar ao carrinho</p>
+                                </MyButton>
+                                <div className='cometairo'>
+                                    <h4>
+                                    "A história entre Kratos e Atreus é uma das melhores que já vi em um jogo. A evolução dos personagens é incrível."
+                                    </h4>
+                                </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
 
-    )
-           
+        )
+            
  }
 
  export default gfw
