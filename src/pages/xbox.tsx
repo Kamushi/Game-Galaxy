3a3820d3055ac382f5cee40e9660952f9dbc12e6
JOAO VICTOR SILVA <joo.34907@aluno.pr.senac.br>
2024-06-14 08:39:08 -0300

    14/06/2024/

--- src/pages/xbox.tsx
+++ src/pages/xbox.tsx
@@ -3,11 +3,9 @@ import helo from '../res/halo.jpg'
 import helo3 from '../res/halo 3.jpg'
 function Xbox (){
     return(
-        <div className='body01'>
         <div className='jogos'>
         <a className='card' style={{background: `url(${helo })`, backgroundSize: `cover`}} href='/halo'/>
         <a className='card' style={{background: `url(${helo3 })`, backgroundSize: `cover`}} href='/halo3'/>
-        </div>
         </div>
     )
 }
